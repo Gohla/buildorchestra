93fad69e1ee5d381baf3d7ad2604e619f6660f61
Gabriel Konat <gabrielkonat@gmail.com>
2016-07-01 17:22:32 +0200

    Use semantic versioning scheme.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name='buildorchestra',
-  version='0.1',
+  version='0.1.0',
   description='Build system orchestration tool',
   url='http://github.com/Gohla/buildorchestra',
   author='Gabriel Konat',
